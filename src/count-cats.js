1e8bc0b7fa968920accd1a10afa6cb8635a1eedb
Pavel703 <43139617+Pavel703@users.noreply.github.com>
2020-03-04 00:00:11 +0200

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let number = 0;
+
+  backyard.filter(arr => arr.filter(item => {
+    if (item === '^^') number++;
+  }));
+
+  return number;
 };
